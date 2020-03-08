f933af42de86aba497dfb403b65ef264a3848aea
yuvaraj <shivarajnaidu@gmail.com>
2020-03-08 15:07:02 +0530

    added build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+# npm version [<newversion> | major | minor | patch | premajor | preminor | prepatch | prerelease [--preid=<prerelease-id>] | from-git]
+
+cd ./projects/ng-marquee
+npm version minor
+
+cd ../../
+npm version minor
+
+ng build ng-marquee
+
+rm README.md
+cp ./projects/ng-marquee/README.md README.md
